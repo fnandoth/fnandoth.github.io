43320206407bc2427ee2346e853fa3aa908cfa03
fnandoth <fernandoarielthielelepe@gmail.com>
2025-11-03 18:34:05 -0300

    fix: add vite alias configuration for components, styles, and layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,13 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://fnandoth.github.io',
+  vite: {
+    resolve: {
+      alias: {
+        '@components': '/src/components',
+        '@styles': '/src/styles',
+        '@layouts': '/src/layouts'
+      }
+    }
+  }
 });
